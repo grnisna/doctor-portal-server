5aad3dd11724b34f3b6f5d9c75ad676b10512bb2
nisanAhmed <mdnisanahmed63@gmail.com>
2022-05-18 17:48:01 +0600

    appoin tsend user email

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
+const nodemailer = require("nodemailer");
 const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const verify = require('jsonwebtoken/verify');
@@ -12,9 +13,49 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// ---------mongo db connection -----------
+// ---------------node mailer-----------------------------------------
+// -------------------------------------------------------------------
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'mdnisanahmed63@gmail.com',
+      pass: 'lmjtqgnszmcdnyef'
+    }
+  });
+
+const sendAppointmentEmail = booking =>{
+    const {patient,patientName,date,slot,treatement} = booking;
+    var mailOptions = {
+        from: 'mdnisanahmed63@gmail.com',
+        to: patient,
+        subject: `Appointment is Comfirm on ${date} at ${slot}`,
+        text: `Confirm you appointment on ${date} at ${slot} `,
+        html:`<div>
+            <h2>Hello ${patientName},</h2>
+            <h4>Your appointment is confirm in <strong> ${treatement}</strong> on ${date} at ${slot} </h4> 
+            <p>
+            Conscious of its spiritual and moral heritage, the Union is founded on the indivisible, universal values of human dignity, freedom, equality and solidarity; it is based on the principles of democracy and the rule of law. It places the individual at the heart of its activities, by establishing the citizenship of the Union and by creating an area of freedom, security and justice.
+            </p>
+        </div>`
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+}
+
+  
 
 
+//   ------------------- send appoinment email to user----------------
+// 
+// ----------------------------------------------------------------------------------
+
+// ---------mongo db connection ----------- 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kemvm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 // -----------------secure user access by token ----------------
@@ -43,11 +84,24 @@ async function run() {
         const serviceCollection = client.db("doctor_portal").collection("service");
         const bookingCollection = client.db("doctor_portal").collection("booking");
         const userCollection = client.db("doctor_portal").collection("users");
+        const doctorCollection = client.db('doctor_portal').collection('doctors');
+
+        // ----------------verify admin ----------------------------
+        const verifyAdmin = async (req, res, next) => {
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                next();
+            }
+            else {
+                res.status(403).send({ message: 'forbidden Access' })
+            }
+        }
 
         // --------get all services from mongodeb and send client ------ 
         app.get('/service', async (req, res) => {
             const query = {};
-            const cursor = serviceCollection.find(query);
+            const cursor = serviceCollection.find(query).project({ name: 1 });
 
             const services = await cursor.toArray();
             res.send(services);
@@ -84,32 +138,27 @@ async function run() {
         });
 
         // -------------- make role in admin ---------- 
-        app.put('/user/admin/:email',varifyJWT,async(req,res)=>{
+        app.put('/user/admin/:email', varifyJWT,verifyAdmin, async (req, res) => {
             const email = req.params.email;
-            const requester = req.decoded.email;
-            const requesterAccount = await userCollection.findOne({email:requester});
-            if(requesterAccount.role === 'admin'){
-                const filter ={email:email};
-                const updateDoc ={ $set:{role:'admin'}};
-                const result = await userCollection.updateOne(filter,updateDoc);
-                return res.send(result);
-            }
-            else{
-                return res.status(403).send({message:'forbidden'});
-            }
+                const filter = { email: email };
+                const updateDoc = { $set: { role: 'admin' } };
+                const result = await userCollection.updateOne(filter, updateDoc);
+                 res.send(result);        
+
+
         });
 
         // -------------------- check admin ====------------------ 
-        app.get('/admin/:email', async(req,res)=>{
+        app.get('/admin/:email', async (req, res) => {
             const email = req.params.email;
-            const user = await userCollection.findOne({email:email});
+            const user = await userCollection.findOne({ email: email });
             const isAdmin = user.role === 'admin';
-            res.send({admin:isAdmin});
-        
+            res.send({ admin: isAdmin });
+
         })
 
         // ============== get user from mongoDB for admin ========= 
-        app.get('/user', varifyJWT, async(req,res)=>{
+        app.get('/user', varifyJWT, async (req, res) => {
             const users = await userCollection.find().toArray();
             res.send(users);
         })
@@ -124,8 +173,8 @@ async function run() {
                 const bookings = await bookingCollection.find(query).toArray();
                 return res.send(bookings);
             }
-            else{
-                return res.status(403).send({message:'forbidden'})
+            else {
+                return res.status(403).send({ message: 'forbidden' })
             }
         });
 
@@ -140,7 +189,31 @@ async function run() {
             }
 
             const result = await bookingCollection.insertOne(booking);
+            // send appoint email to user ----------
+            sendAppointmentEmail(booking);
             return res.send({ success: true, result });
+        });
+
+
+        // ------------------ set doctor info to mongoDB -------------/ 
+        app.post('/doctor',varifyJWT,verifyAdmin, async (req, res) => {
+            const doctor = req.body;
+            const result = await doctorCollection.insertOne(doctor);
+            res.send(result);
+        });
+
+        // -------------------- get doctors data for client site --------- 
+        app.get('/doctor',varifyJWT,verifyAdmin,async(req,res)=>{
+            const doctors = await doctorCollection.find().toArray();
+            res.send(doctors);
+        });
+
+        // ------------ delele one doctor from mongoDB by client site -------------
+        app.delete('/doctor/:email',varifyJWT,verifyAdmin,async(req,res)=>{
+            const email = req.params.email;
+            const filter = {email:email};
+            const result = await doctorCollection.deleteOne(filter);
+            res.send(result); 
         });
 
 
